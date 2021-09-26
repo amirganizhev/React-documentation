c89c846e6840e31e15d0416b437b2d59be662c35
Amir <ganizhev.amir@mail.ru>
2021-09-27 02:05:26 +0300

    Props - аргументы компонента

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, {useState} from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import './styles/App.css'
+import PostList from './components/PostList';
 
 function App() {
 
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'description'},
+    {id: 2, title: 'Javascript 2', body: 'description'},
+    {id: 3, title: 'Javascript 3', body: 'description'},
+  ])
+
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'description'},
+    {id: 2, title: 'Python 2', body: 'description'},
+    {id: 3, title: 'Python 3', body: 'description'},
+  ])
+
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+      <PostList posts={posts} title='Посты про Javascript'/>
+      <PostList posts={posts2} title='Посты про Python'/>
     </div>
   );
 
