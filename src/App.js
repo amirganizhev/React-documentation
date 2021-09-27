b54cb1066392d76b61fb04961fabc1aa1cdba3fb
Amir <ganizhev.amir@mail.ru>
2021-09-27 20:41:28 +0300

    Props.children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './styles/App.css'
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
 
@@ -10,16 +11,14 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'description'},
   ])
 
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python', body: 'description'},
-    {id: 2, title: 'Python 2', body: 'description'},
-    {id: 3, title: 'Python 3', body: 'description'},
-  ])
-
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Название поста"/>
+        <input type="text" placeholder="Описание поста"/>
+        <MyButton>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title='Посты про Javascript'/>
-      <PostList posts={posts2} title='Посты про Python'/>
     </div>
   );
 
