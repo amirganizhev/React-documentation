ec4b10f40952943951e8bae2c0f6d5224b807a24
Amir <ganizhev.amir@mail.ru>
2021-09-28 02:07:09 +0300

    useRef() неуправляемый компонент

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './styles/App.css'
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
 
@@ -11,12 +12,31 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'description'},
   ])
 
+  const [title, setTitle] = useState('')
+
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title)
+    console.log(bodyInputRef.current.value)
+  }
+
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Название поста"/>
-        <input type="text" placeholder="Описание поста"/>
-        <MyButton disabled>Создать пост</MyButton>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title='Посты про Javascript'/>
     </div>
