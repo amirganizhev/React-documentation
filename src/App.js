bc52e0b1367e8d73df73c8e2f949de485c38d8d7
Amir <ganizhev.amir@mail.ru>
2021-10-06 04:58:06 +0300

    Кастомные хуки

--- src/App.js
+++ src/App.js
@@ -7,29 +7,15 @@ import MySelect from './components/UI/select/MySelect';
 import MyInput from './components/UI/input/MyInput';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'Adffd', body: 'g'},
-    {id: 2, title: 'fddfb', body: 'htion'},
-    {id: 3, title: 'bgbfgb', body: 'descftion'},
-  ])
+  const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({sort: '', query: ''})
-
   const[modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if(filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
