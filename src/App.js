b620666a617e770877612c32dcd4372632c50612
Amir <ganizhev.amir@mail.ru>
2021-10-08 09:51:35 +0300

    react-router

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
 import React, {useState, useMemo, useEffect} from 'react';
 import './styles/App.css'
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import PostFilter from './components/PostFilter';
-import MySelect from './components/UI/select/MySelect';
-import MyInput from './components/UI/input/MyInput';
-import MyModal from './components/UI/MyModal/MyModal';
-import MyButton from './components/UI/button/MyButton';
-import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
+import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom';
+import About from './pages/About'
+import Posts from './pages/Posts'
+import Error from './pages/Error'
+import Navbar from './components/UI/Navbar/Navbar'
+import AppRouter from './components/AppRouter'
 
 function App() {
-
-  const [posts, setPosts] = useState([])
-
-  const [filter, setFilter] = useState({sort: '', query: ''})
-  const[modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-  async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
-  }
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
   return (
-    <div className="App">
-      <button onClick={fetchPosts}>GET POSTS</button>
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-        Создать пользователя
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}
-      />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про Javascript'/>
-    </div>
-  );
-
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const COMPONENT = () => {
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>
+        Пользователь перешел на несуществующую страницу!
+      </h1>
+    </div>
+  )
+}
+
+export default COMPONENT;
