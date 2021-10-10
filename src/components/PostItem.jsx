f132d7b57332f6e99a84c27f0773c1e509499bda
Amir <ganizhev.amir@mail.ru>
2021-10-10 09:12:07 +0300

    Динамическая навигация. useHistory(), useParams()

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import MyButton from './UI/button/MyButton';
+import MyButton from "./UI/button/MyButton";
+import {useHistory} from 'react-router-dom';
 
 const PostItem = (props) => {
-  return (
-    <div className="post">
-      <div className="post__content">
-        <strong>{props.post.id}. {props.post.title}</strong>
-        <div>{props.post.body}</div>
-      </div>
-        <div className="post__btns">
-          <MyButton onClick={() => props.remove(props.post)}>
-            Удалить
-          </MyButton>
+    const router = useHistory()
+
+    return (
+        <div className="post">
+            <div className="post__content">
+                <strong>{props.post.id}. {props.post.title}</strong>
+                <div>
+                    {props.post.body}
+                </div>
+            </div>
+            <div className="post__btns">
+                <MyButton onClick={() => router.push(`/posts/${props.post.id}`)}>
+                    Открыть
+                </MyButton>
+                <MyButton onClick={() => props.remove(props.post)}>
+                    Удалить
+                </MyButton>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
 export default PostItem;
