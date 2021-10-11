07bb9af61215262a6abe51258925c8ee97307ad6
Amir <ganizhev.amir@mail.ru>
2021-10-11 03:35:27 +0300

    useContext() - глобальные данные

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, {useState, useMemo, useEffect} from 'react';
 import './styles/App.css'
 import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom';
-import About from './pages/About'
-import Posts from './pages/Posts'
-import Error from './pages/Error'
-import Navbar from './components/UI/Navbar/Navbar'
-import AppRouter from './components/AppRouter'
+import Navbar from './components/UI/Navbar/Navbar';
+import AppRouter from './components/AppRouter';
+import {AuthContext} from './context';
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setIsAuth(true)
+        }
+        setLoading(false);
+    }, [])
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+      isAuth,
+      setIsAuth,
+      isLoading
+    }}>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,38 +1,44 @@
-import React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
-import About from '../pages/About';
-import Posts from '../pages/Posts';
-import Error from '../pages/Error';
-import PostIdPage from '../pages/PostIdPage';
-import {privateRoutes, publicRoutes} from '../router';
+import React, {useContext} from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
+import {privateRoutes, publicRoutes} from "../router";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-  const isAuth = true;
-  return (
-    isAuth
-      ?
-      <Switch>
-        {privateRoutes.map(route =>
-          <Route
-            component={route.component}
-            path={route.path}
-            exact={route.exact}
-          />
-        )}
-        <Redirect  to="/posts"/>
-      </Switch>
-      :
-      <Switch>
-        {publicRoutes.map(route =>
-          <Route
-            component={route.component}
-            path={route.path}
-            exact={route.exact}
-          />
-        )}
-        <Redirect  to="/posts"/>
-      </Switch>
-  )
-}
+    const {isAuth, isLoading} = useContext(AuthContext);
+    console.log(isAuth)
+
+    if (isLoading) {
+        return <Loader/>
+    }
+
+    return (
+        isAuth
+            ?
+            <Switch>
+                {privateRoutes.map(route =>
+                    <Route
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                        key={route.path}
+                    />
+                )}
+                <Redirect to='/posts'/>
+            </Switch>
+            :
+            <Switch>
+                {publicRoutes.map(route =>
+                    <Route
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                        key={route.path}
+                    />
+                )}
+                <Redirect to='/login'/>
+            </Switch>
+    );
+};
 
 export default AppRouter;
