22e163fab855eb7509d13ba06461ebd464911cfc
Amir <ganizhev.amir@mail.ru>
2021-10-11 04:37:20 +0300

    Реализовал некоторые фичи

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React, {useState, useMemo, useEffect} from 'react';
-import './styles/App.css'
-import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom';
-import Navbar from './components/UI/Navbar/Navbar';
-import AppRouter from './components/AppRouter';
-import {AuthContext} from './context';
+import React, {useEffect, useState} from 'react';
+import './styles/App.css';
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
+import {AuthContext} from "./context";
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setLoading] = useState(true);
 
-  const [isAuth, setIsAuth] = useState(false);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
+    useEffect(() => {
         if (localStorage.getItem('auth')) {
             setIsAuth(true)
         }
         setLoading(false);
     }, [])
 
-  return (
-    <AuthContext.Provider value={{
-      isAuth,
-      setIsAuth,
-      isLoading
-    }}>
-      <BrowserRouter>
-        <Navbar />
-        <AppRouter />
-      </BrowserRouter>
-    </AuthContext.Provider>
-  )
+    return (
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading
+        }}>
+            <BrowserRouter>
+                <Navbar/>
+                <AppRouter/>
+            </BrowserRouter>
+        </AuthContext.Provider>
+    )
 }
 
 export default App;
